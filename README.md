e12a91e40def06fcd0293c044db7e8733561a8a6
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2024-10-08 18:48:05 -0300

    finaliza a implementação do codigo

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Para exemplificação, utilizarem um sistema de banco, onde uma conta poderá re
 
 A ideia e mostrar como a falta de sincronização entre as threads podem causar inconsistencias nos dados.
 
-Reláotorio:
+Relátorio:
 
 Código sem Mutexes e semaforos:
     -É perceptivel que durante a execução do código, as operações de retirada e deposito realizadas concomitantemente pelas threads fazem com que o valor do saldo seja diferente do esperado, pois 2 threads podem ler o mesmo saldo ao mesmo tempo e fazer com que o valor final seja diferente do esperado, gerando assim uma condição de corrida, gerando assim inconsistencia no saldo final.
 
-    
+
 
 
--- com-EM/BancoComMutexes.java
+++ com-EM/BancoComMutexes.java
@@ -66,3 +66,49 @@ class ContaBanco {
     }
 }
 
+public class BancoComMutexes {
+    public static void main(String[] args) throws InterruptedException {
+
+        ContaBanco Conta = new ContaBanco();
+        Thread[] threads = new Thread[10]; //Array de armazenamento das threads
+        Random random = new Random();
+
+        for (int i = 0; i < threads.length; i++) { // vão ser executadas todas as 10 threads armazenadas no array
+
+            threads[i] = new Thread(() -> { //Cria e inicia cada uma das 10 threads dentro da array
+
+                for (int j = 0; j < 1000; j++) { // Para cada threads serão executados 1000 operações na conta do banco
+                    
+                    // 50% de chance para depósito ou retirada
+                   
+                    if (random.nextBoolean()) {
+                        Conta.deposito(10);
+                    } else {
+                        Conta.retirada(10);
+                    }
+
+                    //Esse try vai criar um atraso de execução entre as threads, a ideia é aumentar a chance de conflitos entre as threads, aumentando a taxa de erro
+                    try {
+
+                        Thread.sleep(random.nextInt(10)); // Atraso aleatório
+
+                    } catch (InterruptedException e) {
+
+                        Thread.currentThread().interrupt();
+                        
+                    }
+
+
+                }
+
+            });
+            threads[i].start(); // inicia thread
+        }
+
+        for (Thread thread : threads) { //Aguarda a execução de todas as threads
+            thread.join();
+        }
+
+        System.out.println("Saldo final: " + Conta.getSaldo());
+    }
+}
