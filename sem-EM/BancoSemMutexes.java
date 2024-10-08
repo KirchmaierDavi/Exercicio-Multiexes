33e59bf1b2c44ef0c412a4d181947550798ba3f2
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2024-10-08 17:33:03 -0300

    Finaliza implementação do algoritmo sem mutexes

--- sem-EM/BancoSemMutexes.java
+++ sem-EM/BancoSemMutexes.java
@@ -1,17 +1,19 @@
-
+import java.util.Random;
 
 class ContaBanco {
     private int saldo = 1000; // inicia classe banco com saldo inicial
 
     public void deposito(int quantia) { //Funcao responsavel por realizar o deposito de uma quantia
         saldo += quantia;
+        System.out.println("Depositado: " + quantia + ", Saldo atual: " + saldo);
     }
 
     public void retirada(int quantia) { //Funcao responsavel por sacar certa quantia da conta
         if (saldo >= quantia) {
             saldo -= quantia;
-        } else if (saldo == 0) {
-            System.out.println("seu saldo está zerado, retirada impossível");
+            System.out.println("Retirado: " + quantia + ", Saldo atual: " + saldo);
+        } else {
+            System.out.println("Tentativa de retirada de " + quantia + " falhou. Saldo atual: " + saldo);
         }
     }
 
@@ -19,4 +21,39 @@ class ContaBanco {
         return saldo;
     }
 }
+
+public class BancoSemMutexes {
+    public static void main(String[] args) throws InterruptedException {
+
+        ContaBanco conta = new ContaBanco();
+        Thread[] threads = new Thread[10]; //Array para guardar as threads
+        Random random = new Random();
+
+        for (int i = 0; i < threads.length; i++) { // vão ser executadas todas as 10 threads armazenadas no array
+
+            threads[i] = new Thread(() -> { //Cria e inicia cada uma das 10 threads dentro da array
+
+                for (int j = 0; j < 1000; j++) { // cada thread vai executar 1000 operações de retirada ou inserção de dinheiro
+                    // 50% de chance para depósito ou retirada
+
+                    if (random.nextBoolean()) {
+                        conta.deposito(10);
+                    } else {
+                        conta.retirada(10);
+                    }
+
+                }
+
+            });
+            threads[i].start(); //Inicia a thread 
+        }
+
+        for (Thread thread : threads) { //Aguarda a execução de todas as threads
+            thread.join();
+        }
+
+        System.out.println("Saldo final: " + conta.getSaldo());
+    }
+}
+
 
