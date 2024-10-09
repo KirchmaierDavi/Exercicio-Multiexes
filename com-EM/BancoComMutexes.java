99310d38b643011876d68ded1bd9b9434bc93fe2
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2024-10-08 22:37:05 -0300

    adiciona conta para comparar valor esperado com saldo final

--- com-EM/BancoComMutexes.java
+++ com-EM/BancoComMutexes.java
@@ -73,6 +73,9 @@ public class BancoComMutexes {
         Thread[] threads = new Thread[10]; //Array de armazenamento das threads
         Random random = new Random();
 
+        final int[] num_adicoes = {0};
+        final int[] num_retiradas = {0};
+
         for (int i = 0; i < threads.length; i++) { // vão ser executadas todas as 10 threads armazenadas no array
 
             threads[i] = new Thread(() -> { //Cria e inicia cada uma das 10 threads dentro da array
@@ -82,9 +85,17 @@ public class BancoComMutexes {
                     // 50% de chance para depósito ou retirada
                    
                     if (random.nextBoolean()) {
+
                         Conta.deposito(10);
+
+                        num_adicoes[0]++;
+
                     } else {
+
                         Conta.retirada(10);
+
+                        num_retiradas[0]++;
+
                     }
 
                     //Esse try vai criar um atraso de execução entre as threads, a ideia é aumentar a chance de conflitos entre as threads, aumentando a taxa de erro
@@ -109,6 +120,21 @@ public class BancoComMutexes {
             thread.join();
         }
 
+        int totalDepositos = num_adicoes[0] * 10;
+        int totalRetiradas = num_retiradas[0] * 10;
+
+        // Calcular o valor esperado
+        int valorEsperado = 1000 + totalDepositos - totalRetiradas;
+
+        // Garante que o valor esperado não seja negativo
+        if (valorEsperado < 0) {
+
+            valorEsperado = 0;
+
+        }
+
         System.out.println("Saldo final: " + Conta.getSaldo());
+        System.out.println("Valor esperado:" + valorEsperado);
+
     }
 }
